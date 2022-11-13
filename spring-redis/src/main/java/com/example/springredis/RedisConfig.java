2ad990600739fbdc8aa8c615fbef8843437b8e52
Rossi-9999 <hieu2233ckh@gmail.com>
2022-11-13 16:58:42 +0700

    cau1

---
+++ spring-redis/src/main/java/com/example/springredis/RedisConfig.java
@@ -0,0 +1,45 @@
+package com.example.springredis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+    @Value("${redis.host}")
+    private String redisHost;
+
+    @Value("${redis.port}")
+    private int redisPort;
+
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        // Tạo Standalone Connection tới Redis
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(redisHost, redisPort));
+    }
+
+    @Bean
+    @Primary
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        // tạo ra một RedisTemplate
+        // Với Key là Object
+        // Value là Object
+        // RedisTemplate giúp chúng ta thao tác với Redis
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
+}
+
